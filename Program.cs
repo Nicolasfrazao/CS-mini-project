e27c4f5d34075cb03e18db48bba26f071f62efbb
Nick Frazao <nicolas_frazao@hotmail.com>
2024-10-12 18:55:46 -0300

    new branch test

--- Program.cs
+++ Program.cs
@@ -10,80 +10,93 @@ public class TurnOff
         /// </summary>
         public static void Main()
         {
-            // Call the Welcome() method to start the program
-            Welcome();
         }
 
-        /// <summary>
-        /// This function takes a selected time string and compares it to the current time.
-        /// If the two times are equal, the program will shut down the computer.
-        /// </summary>
-        /// <param name="selected_time">The time selected by the user in HH:mm:ss format</param>
-        public static void TurnOffComputer(string selected_time)
-        {
-            var targetTime = DateTime.Parse(selected_time);
-            var timeDifference = targetTime - DateTime.Now;
-            var timeToTurnOff = timeDifference.ToString(@"hh\:mm\:ss");
-            Console.WriteLine($"Time to turn off:{timeToTurnOff}");
-            // Wait until the target time is reached
-            while (DateTime.Now < targetTime)
-            {
-                Thread.Sleep(100);
-                Console.WriteLine($"Time to turn off: {timeDifference}");
-            }
-            Console.WriteLine("Shutting down the computer...");
-            System.Diagnostics.Process.Start("shutdown", "/s /t 0");
+        public static void Start() {
+            
         }
 
-        /// <summary>
-        /// This is the main entry point for the Welcome() method.
-        /// It is called from the Main() method and is responsible for starting the program.
-        /// </summary>
-        /// <remarks>
-        /// This method is the first method that is called when the program is started.
-        /// It is responsible for printing a welcome message to the user and asking them to enter the time in 24-hour format that they want the computer to shut down.
-        /// </remarks>
         public static void Welcome() {
-            // Print a welcome message to the user
-            Console.WriteLine("Welcome to the TurnOffComputer program!");
+        }
 
-            // Ask the user to enter the time in 24-hour format that they want the computer to shut down
-            Console.WriteLine("Select a time to turn off the computer:");
+        pu
+        public static void TurnOffComputer() {
 
-            // Call the setTime() method to get the user's input for the time to shut down the computer
-            setTime();
         }
 
 
-        /// <summary>
-        /// This is the main entry point for the setTime() method.
-        /// It is called from the Welcome() method and is responsible for getting the user's input for the time to shut down the computer.
-        /// </summary>
-        public static void setTime() {
-            // Print a message to the user asking them to enter the time in 24-hour format
-            Console.Write("Enter the time in 24-hour format (HH:mm): ");
 
-            // Read the user's input in one line
-            #pragma warning disable CS8602 // Dereference of a possibly null reference.
-            string[] input = Console.ReadLine().Split(':');
-            #pragma warning restore CS8602 // Dereference of a possibly null reference.
-
-            // Check if the input is valid
-            if (input.Length == 2 && int.TryParse(input[0], out int hour) && int.TryParse(input[1], out int minute) && hour >= 0 && hour < 24 && minute >= 0 && minute < 60) {
-                // Concatenate the hour and minute to get the selected time in the format "HH:mm:ss"
-                string selected_time = $"{hour:D2}:{minute:D2}:00";
-
-                // Print the selected time to the user
-                Console.WriteLine($"Selected time: {selected_time}");
-
-                // Call the TurnOffComputer() method and pass the selected time as an argument
-                TurnOffComputer(selected_time);
+        public static void RegisterInSystem() {
+            // Check if the program is already registered in the system
+            //##TODO
+            /*
+            if (IsRegistered()) {
+                Console.WriteLine("The program is already registered in the system.");
+                return;
             }
-            else {
-                Console.WriteLine("Invalid input. Please enter the time in 24-hour format (HH:mm).");
-                setTime();
-            }
+
+            // Register the program in the system
+            Register();
+            */
         }
     }
 }
 
+/*
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string placeholder = "HH : MM";
+        Console.Write(placeholder);
+
+        // Move cursor to start of placeholder
+        int startPosition = Console.CursorLeft - placeholder.Length;
+        Console.SetCursorPosition(startPosition, Console.CursorTop);
+
+        string timeInput = ReadTimeInputWithPlaceholder(placeholder);
+        
+        Console.WriteLine("\nYou entered: " + timeInput);
+    }
+
+    static string ReadTimeInputWithPlaceholder(string placeholder)
+    {
+        ConsoleKeyInfo key;
+        string userInput = string.Empty;
+
+        while ((key = Console.ReadKey(intercept: true)).Key != ConsoleKey.Enter)
+        {
+            if (userInput.Length == 0 && key.Key != ConsoleKey.Backspace)
+            {
+                // Clear placeholder text
+                Console.Write(new string(' ', placeholder.Length));
+                Console.SetCursorPosition(Console.CursorLeft - placeholder.Length, Console.CursorTop);
+            }
+
+            if (key.Key == ConsoleKey.Backspace && userInput.Length > 0)
+            {
+                // Handle backspace: Remove last character
+                userInput = userInput.Substring(0, userInput.Length - 1);
+                Console.Write("\b \b"); // Move back, print space to clear, move back again
+            }
+            else if (key.KeyChar >= '0' && key.KeyChar <= '9')
+            {
+                // Allow only digits
+                if (userInput.Length == 2)
+                {
+                    userInput += " : ";
+                    Console.Write(" : ");
+                }
+                userInput += key.KeyChar;
+                Console.Write(key.KeyChar);
+            }
+        }
+
+        return userInput;
+    }
+}
+
+
+*/
--- obj/Debug/net8.0/TurnOFF.AssemblyInfo.cs
+++ obj/Debug/net8.0/TurnOFF.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TurnOFF")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f2a0d9cf67abc4c452f89c396b2fa863b9a655d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+772dcf8588d5bec193dbf14b620d69488e41fd9a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TurnOFF")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TurnOFF")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
